7a39e1165597c39a469d51663ef951fa771e5318
Philip <philiposun@gmail.com>
2023-05-06 14:23:04 -0700

    Update array.php

--- array.php
+++ array.php
@@ -8,11 +8,63 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body >
-       <!-- ARRAY STATEMENTS -->
+    <!-- ARRAY STATEMENTS -->
   
     <?php
-        $cars = array("Benz", "Toyata", "Tesla", "Ford");
-        echo "I like ". $cars[2] . " more, but i can buy " . $cars[0] . ", and " .$cars[3] . " if i want to buy it.";
+        $cars = array("Benz",
+         "Toyata",
+          "Tesla",
+           "Ford");
+        echo "I like ". $cars[2] . " more, but i can buy " . $cars[0] . ", and " .$cars[3] . " if i want to buy it. <br> <br>";
     ?>
+
+    <!-- Get The Length of an Array  -->
+      
+    <?php
+        $sub = array(
+            "chemistry",
+             "biology", 
+             "maths", 
+             "english" 
+        );
+        echo count($sub);
+        echo "<br> <br>";
+    ?>
+
+    <!-- PHP Indexed Arrays -->
+    <?php
+        //    loop through an index array
+
+        $names = array("philip", "john", "nicholas", "ope");
+        $arrlength = count($names);
+
+        for ($i=0; $i < $arrlength; $i++) { 
+            echo $names[$i];
+            echo "<br> ";
+            echo "<br> ";
+        }
+    ?>
+
+    <!-- PHP Associative Arrays -->
+    <?php
+    $age = array(
+        "Peter"=>"35",
+        "Ben"=>"37",
+        "Joe"=>"43");
+
+        echo "Peter is " . $age['Peter'] . " years old.";
+        echo "<br> <br>";
+    ?>
+
+    <?php
+        //    loop through an associative array
+        $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+        foreach ($age as $x => $x_value) {
+            echo "key=" . $x . ", value=" . $x_value;
+            echo "<br> <br>";
+        }
+    ?>     
+
 
 </body>
