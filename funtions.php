d8ea5e03e93897e18b947661a6dba3ee1deda2dd
Philip <philiposun@gmail.com>
2023-05-06 13:17:20 -0700

    initial commit

--- funtions.php
+++ funtions.php
@@ -22,16 +22,42 @@
         familyName("job", "2004");
         familyName("john", "2005");
         familyName("kenedy", "2006");
-
         echo "<br>";
-
     ?>
 
     <?php 
-
         function addNumbers (int $a, int $b) {
             return $a + $b; 
         }
-        echo addNumbers(5, "5");
+        echo addNumbers(5, "5") . "<br>" . "<br>";
     ?>
+
+    <?php
+        function sum (int $x, int $y) {
+            $z = $x + $y;
+            return $z;
+        }   
+        echo "5 + 10 = " . sum(5, 10) . "<br>";
+        echo "5 + 10 = " . sum(4, 8) . "<br>";
+        echo "5 + 10 = " . sum(3, 6) . "<br>" . "<br>";
+    ?>
+
+    <?php 
+        function Numbers(float $c, float $d) {
+            return $c + $d;
+        }
+        echo addNumbers(1.2, 5.2) . "<br>" . "<br>";;
+    ?>
+
+    <?php
+        function add_five(&$value) {
+        $value += 5;
+        }
+
+        $num = 2;
+        add_five($num);
+        echo $num;
+    ?>
+
+
 </body>    
