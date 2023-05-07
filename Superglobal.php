0945c192d5b5642bf9e85b5cd9078cd5b20471f1
Philip <philiposun@gmail.com>
2023-05-07 14:42:43 -0700

    initial commit

--- Superglobal.php
+++ Superglobal.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body >
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name : <input type="text" name="fname">
+        <input type="submit">
+    </form>
+<br>
+
     <!-- PHP Superglobal - $GLOBALS -->
 
     <?php
@@ -20,8 +27,40 @@
         
         addition();
         echo $z;
-    
+        echo "<br> <br>";
+    ?>
 
+    <!-- PHP Superglobal - $_SERVER -->
+
+    <?php
+        echo $_SERVER['PHP_SELF'];
+        echo "<br>";
+        echo $_SERVER['SERVER_NAME'];
+        echo "<br>";
+        echo $_SERVER['HTTP_HOST'];
+        echo "<br>";
+        echo $_SERVER['HTTP_REFERER'];
+        echo "<br>";
+        echo $_SERVER['HTTP_USER_AGENT'];
+        echo "<br>";
+        echo $_SERVER['SCRIPT_NAME'];
+        echo "<br>";
+        echo $_SERVER['SERVER_ADDR'];
+        echo "<br> <br>";
+    ?>
+
+    <!-- PHP Superglobal - $_REQUEST -->
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $name = htmlspecialchars($_REQUEST['fname']);
+        if (empty($name)) {
+            echo "Name is empty"; 
+        } else {
+            echo $name;
+        }
+    }
     ?>
 
 </body>    
