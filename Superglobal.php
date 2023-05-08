4c3cb607c0e309ae5eb85e3a87525d2c0b258b53
Philip <philiposun@gmail.com>
2023-05-08 13:22:01 -0700

    initial commit

--- Superglobal.php
+++ Superglobal.php
@@ -9,10 +9,6 @@
 </head>
 <body >
 
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Name : <input type="text" name="fname">
-        <input type="submit">
-    </form>
 <br>
 
     <!-- PHP Superglobal - $GLOBALS -->
@@ -51,16 +47,41 @@
 
     <!-- PHP Superglobal - $_REQUEST -->
 
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name : <input type="text" name="fname">
+        <input type="submit">
+    </form>
+
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // collect value of input field
-        $name = htmlspecialchars($_REQUEST['fname']);
-        if (empty($name)) {
-            echo "Name is empty"; 
-        } else {
-            echo $name;
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // collect value of input field
+            $name = htmlspecialchars($_REQUEST['fname']);
+            if (empty($name)) {
+                echo "Name is empty"; 
+            } else {
+                echo $name;
+                echo "<br> <br>";
+            }
         }
-    }
+    ?>
+
+    <!-- PHP Superglobal - $_POST -->
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Name: <input type="text" name="lname">
+        <input type="submit">
+    </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // collect value of input field
+            $name = $_POST['lname'];
+            if (empty($name)) {
+                echo "Name is empty";
+            } else {
+                echo $name;
+            }
+        }
     ?>
 
 </body>    
