8a1af93326c651ec32fca645288f02ae76ccca98
Philip <philiposun@gmail.com>
2023-05-10 12:33:20 -0700

    initail commit

--- index1.php
+++ index1.php
@@ -8,38 +8,38 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body >
- <?php
-        $myName = "philip osunlola";
-        $myAge = "20";
-        echo $myName; echo "<br>";
-        echo $myAge;   echo "<br>";
+    <?php
+            $myName = "philip osunlola";
+            $myAge = "20";
+            echo $myName; echo "<br>";
+            echo $myAge;   echo "<br>";
 
-        function myTest() {
-        $x = 5;
-        $y = 4;
-        $z = $x + $y;
-        echo "<p>The addition of variables is :$z </p>";
-        };
-        myTest();
- ?>
-    
- <?php
+            function myTest() {
+            $x = 5;
+            $y = 4;
+            $z = $x + $y;
+            echo "<p>The addition of variables is :$z </p>";
+            };
+            myTest();
+    ?>
+        
+    <?php
 
-        $name = "philip";
-        $surname = "osunlola";
-        Function myWord() {
-            
-        };
-        myWord();
-        echo "<br>";
-        echo $name; echo "<br>";
-        echo $surname;
-        echo "<br>";
+            $name = "philip";
+            $surname = "osunlola";
+            Function myWord() {
                 
-?>
+            };
+            myWord();
+            echo "<br>";
+            echo $name; echo "<br>";
+            echo $surname;
+            echo "<br>";
+                    
+    ?>
 
-          <!-- Global Scope -->
- <?php
+            <!-- Global Scope -->
+    <?php
         $num1 = 4;
         $num2 = 8;
 
@@ -51,126 +51,128 @@
         myFunction();
         echo $num1;
 
- ?>
+    ?>
 
-  <!-- LOOCAL SCOPE -->
-<?php
-    function myFunction1() {
-    $x = 5; // local scope
-    echo "<p>Variable x inside function is: $x</p>";
+    <!-- LOOCAL SCOPE -->
+    <?php
+        function myFunction1() {
+        $x = 5; // local scope
+        echo "<p>Variable x inside function is: $x</p>";
+        }
+        myFunction1();
+    ?>
+
+
+    <!-- The static Keyword -->
+    <?php
+    function myTest1() {
+        static $x = 0;
+        echo $x;
+    $x++;
     }
-    myFunction1();
-?>
 
+        myTest1();
+        echo "<br>";
+        myTest1();
+        echo "<br>";
+        myTest1();
+    ?>
 
-  <!-- The static Keyword -->
-<?php
-function myTest1() {
-  static $x = 0;
-  echo $x;
-  $x++;
-}
+        <!--THE ECHO STATEMENT  -->
+        
+    <?php
+        echo "<h1>PHP CAN BE FUN </h1> <br>";
+        echo "HELLO WORLD <br>";
+        echo " I'M GOING TO MAKE3 MONEY IS THIS LIFE <br>";
+        echo "I WILL NEVER BE POOR <br>";
 
-    myTest1();
-    echo "<br>";
-    myTest1();
-    echo "<br>";
-    myTest1();
-?>
+        $txt1 = "i want to learn php";
+        $txt2 = "i want to make money that's why sha";
+        $x= 5;
+        $y= 6;
 
-    <!--THE ECHO STATEMENT  -->
+        echo "<h3>  $txt1 </h3> ";
+        echo "why i want to learn php is because " . $txt2 . "<br>";
+        echo $x * $y ;
+
+    ?>
     
-<?php
-    echo "<h1>PHP CAN BE FUN </h1> <br>";
-    echo "HELLO WORLD <br>";
-    echo " I'M GOING TO MAKE3 MONEY IS THIS LIFE <br>";
-    echo "I WILL NEVER BE POOR <br>";
+        <!-- PRINT STATEMENT -->
 
-    $txt1 = "i want to learn php";
-    $txt2 = "i want to make money that's why sha";
-    $x= 5;
-    $y= 6;
+    <?php
+        print "<h1>PHP CAN BE FUN </h1> <br>";
+        print "HELLO WORLD <br>";
+        print" I'M GOING TO MAKE3 MONEY IS THIS LIFE <br>";
+        print "I WILL NEVER BE POOR <br>";
 
-    echo "<h3>  $txt1 </h3> ";
-    echo "why i want to learn php is because " . $txt2 . "<br>";
-    echo $x * $y ;
+        $txt1 = "i want to learn php";
+        $txt2 = "i want to make money that's why sha";
+        $x= 5;
+        $y= 6;
 
-?>
-   
-     <!-- PRINT STATEMENT -->
+        print "<h3>  $txt1 </h3> ";
+        print "why i want to learn php is because " . $txt2 . "<br>";
+        print $x * $y ; 
+        echo "<br>";
 
-<?php
-    print "<h1>PHP CAN BE FUN </h1> <br>";
-    print "HELLO WORLD <br>";
-    print" I'M GOING TO MAKE3 MONEY IS THIS LIFE <br>";
-    print "I WILL NEVER BE POOR <br>";
+    ?>
 
-    $txt1 = "i want to learn php";
-    $txt2 = "i want to make money that's why sha";
-    $x= 5;
-    $y= 6;
+        <!--INTEGER DATA TYPE -->
+    <?php
+        $x = 90775;
+        var_dump($x);
+        echo "<br>"
+    ?>
 
-    print "<h3>  $txt1 </h3> ";
-    print "why i want to learn php is because " . $txt2 . "<br>";
-    print $x * $y ; 
-    echo "<br>";
+    <?php  
+        $cars = array("Volvo","BMW","Toyota");
+        var_dump($cars);
+        echo "<br>";
+        echo "<br>"
+    ?> 
 
-?>
+    <!-- string statements -->
+    <?php
+        echo str_replace("world", "Dolly", "Hello world!");
+        echo "<br>"
+    ?> 
 
-    <!--INTEGER DATA TYPE -->
-<?php
-    $x = 90775;
-    var_dump($x);
-    echo "<br>"
-?>
+    <?php
+        echo strpos("Hello world!", "world"); // outputs 6
+        echo "<br>"
+    ?>
 
-<?php  
-$cars = array("Volvo","BMW","Toyota");
-var_dump($cars);
-echo "<br>";
-echo "<br>"
-?> 
+    <!-- PHP CONSTANT -->
+    <?php
+        define("Welcome", "I will love to meet you");
+        echo Welcome;
 
-   <!-- string statements -->
-<?php
-echo str_replace("world", "Dolly", "Hello world!");
-echo "<br>"
-?> 
+        echo "<br>";
+        echo "<br>";
 
-<?php
-echo strpos("Hello world!", "world"); // outputs 6
-echo "<br>"
-?>
+        // CONSTANT ARRAY
+        define("Cars", [
+            "Toyota",
+            "Telsa",
+            "Ford"
+        ]);
+        echo Cars[2];
 
-<!-- PHP CONSTANT -->
-<?php
-define("Welcome", "I will love to meet you");
-echo Welcome;
+        echo "<br>";
+        echo "<br>";
 
-echo "<br>";
-echo "<br>";
+        define("GREETINGS", "This is my php case study");
 
-   // CONSTANT ARRAY
-define("Cars", [
-    "Toyota",
-    "Telsa",
-    "Ford"
-]);
-echo Cars[2];
+        function myArray () {
+            echo GREETINGS;
+        }
+        myArray();
 
-echo "<br>";
-echo "<br>";
+        echo "<br>";
+        echo "<br>";
+    ?>
 
-define("GREETINGS", "This is my php case study");
-
-function myArray () {
-    echo GREETINGS;
-}
-myArray();
-
-echo "<br>";
-echo "<br>"
-?>
+    <!-- PHP Regular Expressions -->
 
 </body >
 </html>
