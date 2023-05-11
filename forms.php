6c4466bfe198bc64a585cf0582a7dd4e05eb8d0a
Philip <philiposun@gmail.com>
2023-05-11 14:19:00 -0700

    FORMS

--- forms.php
+++ forms.php
@@ -9,19 +9,55 @@
 </head>
 <body>
 
+    <?php
+        // define variables and set to empty values 
+        $name = $email = $gender = $comment = $website = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = test_input($_POST["name"]);
+            $email = test_input($_POST["email"]);
+            $website = test_input($_POST["website"]);
+            $comment = test_input($_POST["comment"]);
+            $gender= test_input($_POST["gender"]);
+        }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
     <form method="post"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><br><br>
-        Name: <input type="text" name="name"><br><br>
-        E-name: <input type="text" name="email"><br><br>
-        Website: <input type="text" name="website"><br><br>
-        Comment: <textarea type="comment" rows="5" cols="40"></textarea><br><br>
-        Gender:<input type="radio" name="gender" value="female">Female<br><br>
-        <input type="radio" name="gender" value="male">Male<br><br>
-        <input type="radio" name="gender" value="other">Other<br><br>
-
-
+        Name: <input type="text" name="name">
+        <br><br>
+        E-name: <input type="text" name="email">
+        <br><br>
+        Website: <input type="text" name="website">
+        <br><br>
+        Comment: <textarea type="comment" rows="5" cols="40"></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
     </form>
 
     <?php
-       
+        echo "<h2>Your Input:</h2>";
+        echo $name;
+        echo "<br>";
+        echo $email;
+        echo "<br>";
+        echo $website;
+        echo "<br>";
+        echo $comment;
+        echo "<br>";
+        echo $gender;
+        echo "<br>";
     ?>
+
 </body>
