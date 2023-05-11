30727a892dd1e6c23767e55492c0a45ea30a160e
Philip <philiposun@gmail.com>
2023-05-11 15:17:34 -0700

    form

--- forms.php
+++ forms.php
@@ -6,44 +6,115 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .error {color: #FF0000;}
+    </style>
 </head>
 <body>
 
     <?php
         // define variables and set to empty values 
+    //     $name = $email = $gender = $comment = $website = "";
+
+    //     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //         $name = test_input($_POST["name"]);
+    //         $email = test_input($_POST["email"]);
+    //         $website = test_input($_POST["website"]);
+    //         $comment = test_input($_POST["comment"]); 
+    //         $gender= test_input($_POST["gender"]);
+    //     }
+
+    // function test_input($data) {
+    //     $data = trim($data);
+    //     $data = stripcslashes($data);
+    //     $data = htmlspecialchars($data);
+    //     return $data;
+    // }
+    // ?>
+
+    <!-- // <form method="post"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><br><br>
+    //     Name: <input type="text" name="name">
+    //     <br><br>
+    //     E-name: <input type="text" name="email">
+    //     <br><br>
+    //     Website: <input type="text" name="website">
+    //     <br><br>
+    //     Comment: <textarea type="comment" rows="5" cols="40"></textarea>
+    //     <br><br>
+    //     Gender:
+    //     <input type="radio" name="gender" value="female">Female
+    //     <input type="radio" name="gender" value="male">Male
+    //     <input type="radio" name="gender" value="other">Other
+    //     <br><br>
+    //     <input type="submit" name="submit" value="Submit">
+    // </form> -->
+
+    <?php
+        // define variables and set to empty values
+        $nameErr = $emailErr = $genderErr = $commentErr = $websiteErr = "";
         $name = $email = $gender = $comment = $website = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $name = test_input($_POST["name"]);
-            $email = test_input($_POST["email"]);
-            $website = test_input($_POST["website"]);
-            $comment = test_input($_POST["comment"]);
-            $gender= test_input($_POST["gender"]);
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required";
+              } else {
+                $name = test_input($_POST["name"]);
+              }
+              
+              if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+              } else {
+                $email = test_input($_POST["email"]);
+              }
+                
+              if (empty($_POST["website"])) {
+                $websiteErr = "website is required";
+              } else {
+                $website = test_input($_POST["website"]);
+              }
+            
+              if (empty($_POST["comment"])) {
+                $commentErr = "comment is required";
+              } else {
+                $comment = test_input($_POST["comment"]);
+              }
+            
+              if (empty($_POST["gender"])) {
+                $genderErr = "Gender is required";
+              } else {
+                $gender = test_input($_POST["gender"]);
+              }
+            }
+        
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
         }
-
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripcslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
     ?>
 
-    <form method="post"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><br><br>
+    <h2>PHP Form Validation Example</h2>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
         Name: <input type="text" name="name">
-        <br><br>
-        E-name: <input type="text" name="email">
-        <br><br>
+            <span class="error">* <?php echo $nameErr;?></span>
+            <br><br>
+        E-mail: <input type="text" name="email">
+            <span class="error">* <?php echo $emailErr;?></span>
+            <br><br>
         Website: <input type="text" name="website">
-        <br><br>
-        Comment: <textarea type="comment" rows="5" cols="40"></textarea>
-        <br><br>
+            <span class="error"><?php echo $websiteErr;?></span>
+            <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            <br><br>
         Gender:
-        <input type="radio" name="gender" value="female">Female
-        <input type="radio" name="gender" value="male">Male
-        <input type="radio" name="gender" value="other">Other
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
+            <input type="radio" name="gender" value="female">Female
+            <input type="radio" name="gender" value="male">Male
+            <input type="radio" name="gender" value="other">Other
+            <span class="error">* <?php echo $genderErr;?></span>
+            <br><br>
+        <input type="submit" name="submit" value="Submit">  
     </form>
 
     <?php
