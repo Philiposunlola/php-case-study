70d5d05b46e900f286cb7dd6f8a58fe79a46ceec
Philip <philiposun@gmail.com>
2023-05-14 02:44:34 +0100

    commit initial commit

--- date.php
+++ date.php
@@ -37,7 +37,16 @@
 
         // Create a Date From a String With strtotime()
         $d = strtotime("10:30pm April 15 2014");
-        echo "Created date is" . date("Y-m-d h:i:sa", $d) . "<br><br>";
+        echo "Created date is " . date("Y-m-d h:i:sa", $d) . "<br><br>";
+
+        $d=strtotime("tomorrow");
+        echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+        $d=strtotime("next Saturday");
+        echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+        $d=strtotime("+3 Months");
+        echo date("Y-m-d h:i:sa", $d) . "<br>";
     ?>
 
 </body>
