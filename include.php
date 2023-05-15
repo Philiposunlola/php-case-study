bad595ccc7263d284a2d2419b2ba537881798619
Philip <philiposun@gmail.com>
2023-05-15 02:23:53 +0100

    initail commit

--- include.php
+++ include.php
@@ -18,5 +18,6 @@
     <h1>Welcome to my home page</h1>
     <p>The include (or require) statement takes all the text/code/markup that exists in the specified file and copies it into the file that uses the include statement.Including files is very useful when you want to include the same PHP, HTML, or text on multiple pages of a website.</p>
     <p>It is possible to insert the content of one PHP file into another PHP file (before the server executes it), with the include or require statement.</P>
-    
+
+    <?php include 'footer.php'?>
 </body>
--- menu.php
+++ menu.php
@@ -1,7 +1,6 @@
 <?php
-    echo '<a href="https://github.com/Philiposunlola">Home</a> -
-    <a href="https://www.w3schools.com/html">HTML Tutorial</a> -
-    <a href="https://www.w3schools.com/css">CSS Tutorial</a> -
-    <a href="https://www.w3schools.com/js">JavaScript Tutorial</a> -
-    <a href="https://www.w3schools.com/php">PHP Tutorial</a>';
+    echo '<a href="https://github.com/Philiposunlola">Github</a> -
+    <a href="https://twitter.com/osunlola_philip">Twitter</a> -
+    <a href="https://www.linkedin.com/in/philip-osunlola/">Linkedin</a> -
+    <a href="https://instagram.com/philipdcoder">Instagram</a> -';
 ?>
