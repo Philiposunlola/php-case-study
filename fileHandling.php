1dd4d70cc34d5a6ebd8bb76b28627da009a0ce87
Philip <philiposun@gmail.com>
2023-05-19 00:44:52 +0100

    php case study

--- fileHandling.php
+++ fileHandling.php
@@ -14,15 +14,53 @@
 
     <!-- PHP FILE HANDING -->
     <?php
-    echo readfile("webdictionary.txt");
+    echo readfile("webdictionary.txt") . "<br><br>";
     ?>
 
     <!-- PHP File Open/Read/Close -->
     <?php
+
+        // PHP Open File - fopen()
         $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
-        echo fread($myfile,filesize("webdictionary.txt"));
+            echo fread($myfile,filesize("webdictionary.txt"));
+            fclose($myfile);
+            echo "<br><br>";
+
+        // PHP Read Single Line - fgets()
+        $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+            echo fgets($myfile);
+            fclose($myfile);
+            echo "<br><br>";
+
+        // PHP Check End-Of-File - feof()
+        $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+
+            while(!feof($myfile)) {
+                echo fgets($myfile) . "<br>";
+            }
+            fclose($myfile);
+            echo "<br><br>"
+    ?>
+
+
+    <!-- PHP File Create/Write -->
+
+    <?php
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        $txt = "Osunlola Philip\n";
+        fwrite($myfile, $txt);
+        $txt = "Osunlola Nicholas\n";
+        fwrite($myfile, $txt);
+        $txt = "Osunlola John\n";
+        fwrite($myfile, $txt);
+        $txt = "Tunmise Madu\n";
+        fwrite($myfile, $txt);
+        $txt = "Akambe Grace\n";
+        fwrite($myfile, $txt);
         fclose($myfile);
-        ?>
+    ?>
+
+
 </body>    
 
 
