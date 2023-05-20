626570113d9b49543cf5de0d4d4ac7cf1612720d
Philip <philiposun@gmail.com>
2023-05-20 02:32:51 +0100

    php case study

--- fileUpload.php
+++ fileUpload.php
@@ -11,30 +11,87 @@
     </style>
 </head>
 <body>
-    <!-- Create The HTML Form -->
+    
+    <?php
+        
+         // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+          $target_dir = "uploads/";
+          $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+          $uploadOk = 1;
+          $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+          if($check !== false) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+          } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+          }
+        }
+        
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+        }
+        
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+        }
+        
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+          echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+          $uploadOk = 0;
+        }
+        
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+          echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+          } else {
+            echo "Sorry, there was an error uploading your file.";
+          }
+        }
+    ?>
+
+        <!-- Create The HTML Form -->
 
     <form actions="upload.php" method="post" enctype="multipart/form-data">
         select imaage to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" name="Upload Image" id="submit">
     </form>
-    
-    <?php
-       $target_dir = "uploads/";
-       $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-       $uploadOk = 1;
-       $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-       // Check if image file is a actual image or fake image
-       if(isset($_POST["submit"])) {
-         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-         if($check !== false) {
-           echo "File is an image - " . $check["mime"] . ".";
-           $uploadOk = 1;
-         } else {
-           echo "File is not an image.";
-           $uploadOk = 0;
-         }
-       }
-    ?>
     
 </body>
