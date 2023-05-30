a6f9fd8ee1563d665df37411695f7b6046049133
Philip <philiposun@gmail.com>
2023-05-30 01:18:13 +0100

    php case study

--- PHP OOP/inheritance.php
+++ PHP OOP/inheritance.php
@@ -62,6 +62,38 @@
             // Try to call three methods from outside class
             $Bags = new Nike("Nike", "White");  // OK. __construct() is public
             $Bags->message(); // OK. message() is public
+            echo "<br>";
+        ?>
+
+        <!-- PHP - Overriding Inherited Methods -->
+        <?php
+            class shoe {
+                public $name;
+                public $color;
+                public function __construct($name, $color) {
+                    $this->name = $name;
+                    $this->color = $color;
+                }
+                public function intro() {
+                    echo "The fruit is {$this->name} and the color is {$this->color}.";
+                  }
+                }
+                
+                class addidas extends shoe {
+                  public $weight;
+                  public function __construct($name, $color, $weight) {
+                    $this->name = $name;
+                    $this->color = $color;
+                    $this->weight = $weight;
+                  }
+                  public function intro() {
+                    echo "The shoe is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.";
+                  }
+                }
+                
+                $addidas = new addidas("addidas", "black", 50);
+                $addidas->intro();
+            
         ?>
     </body>
 </html>
