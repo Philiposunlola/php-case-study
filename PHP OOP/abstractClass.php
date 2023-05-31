713740ba4e2fbe7d89f0b74050c6f63933d2e3fe
Philip <philiposun@gmail.com>
2023-05-31 01:00:19 +0100

    php case study

--- PHP OOP/abstractClass.php
+++ PHP OOP/abstractClass.php
@@ -13,7 +13,45 @@
     <body>
         <!-- PHP OOP - Abstract Classes And Methods -->
         <?php
-            
+            // parent class
+            abstract class Car {
+                public $name;
+                public function __construct($name) {
+                    $this->name = $name;
+                } abstract public function intro() : string;
+            }
+
+            // Child classes
+            class Audi extends Car {
+                public function intro(): string {
+                    return "Nicholas best car! I'm an $this->name!.";
+                }
+            }
+
+            class Tesla extends Car {
+                public function intro(): string {
+                    return "Philip best car! I'm a $this->name!.";
+                }
+            }
+
+            class Benz extends Car {
+                public function intro(): string {
+                    return "John best car! I'm a $this->name!.";
+                }
+            }
+
+            // Create objects from the child class_exists
+            $audi = new audi ("Audi");
+            echo $audi->intro();
+            echo "<br>"; 
+
+            $tesla = new tesla ("Tesla");
+            echo $tesla->intro();
+            echo "<br>"; 
+
+            $Benz = new benz ("Benz");
+            echo $Benz->intro();
+            echo "<br>"; 
         ?>
     </body>
 </html>
