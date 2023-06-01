b61a1a4da4373eaa4e5542b76e041eb5d11201f1
Philip <philiposun@gmail.com>
2023-06-01 01:01:10 +0100

    static method php

---
+++ PHP OOP/staticMethods.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP OOP</title>
+        <link rel="stylesheet" href="style.css">
+        <style>
+            .error {color: #FF0000;}
+        </style>
+    </head>
+    <body>
+        <!-- PHP - Static Methods -->
+        <?php
+            class greeting {
+                public static function welcome() {
+                    echo "Hello World!";
+                } 
+            }
+
+            // Call atatic method
+            greeting::welcome();
+        ?>
+
+        <!-- PHP - More on Static Methods -->
+        <?php
+            class greetting {
+                public static function {
+                    echo "Hello World!"; 
+                } 
+            }
+
+            public function __construct ();
+            new greeting();
+        ?>
+    </body>
+</html>
