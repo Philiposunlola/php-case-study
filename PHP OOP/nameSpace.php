24f63b9d3eb47ecd6fe1251178019475989d6b7f
Philip <philiposun@gmail.com>
2023-06-04 01:07:49 +0100

    php case study

---
+++ PHP OOP/nameSpace.php
@@ -0,0 +1,34 @@
+<?php
+    namespace Html;
+    class Table  {
+        public $title = "";
+        public $numRows = 0;
+        public function message() {
+            echo "<p>Table '{$this->title}' has '{$this->numRows}' row</p>";
+        }
+    }
+
+    $table = new Table();
+    $table->title = "My table";
+    $table->numRows = 5;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP OOP</title>
+        <link rel="stylesheet" href="style.css">
+        <style>
+            .error {color: #FF0000;}
+        </style>
+    </head>
+    <body>
+        <?php
+            $table->message();
+        ?>
+    </body>
+</html>
