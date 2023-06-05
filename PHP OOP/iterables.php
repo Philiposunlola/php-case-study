2c894acbf8da85bb840584ac7c4efc0789edc32a
Philip <philiposun@gmail.com>
2023-06-05 01:38:25 +0100

    Update iterables.php

--- PHP OOP/iterables.php
+++ PHP OOP/iterables.php
@@ -18,7 +18,66 @@
 
             $arr = ["a", "b", "c"];
             printItersble($arr);
+            echo "<br>";
+
+            // OR
+
+            function getIterable():iterable {
+                return ["d", "e", "f"];
+            }
+
+            $myIterable = getIterable();
+            foreach ($myIterable  as $item) {
+                echo $item;
+            }
           
         ?>
+
+        <!-- PHP - Creating Iterables -->
+        <?php
+// Create an Iterator
+            class MyIterator implements Iterator {
+                private $items = [];
+                private $pointer = 0;
+
+                public function __construct($items) {
+                    // array_values() makes sure that the keys are numbers
+                    $this->items = array_values($items);
+                }
+
+                public function current() {
+                    return $this->items[$this->pointer];
+                }
+
+                public function key() {
+                    return $this->pointer;
+                }
+
+                public function next() {
+                    $this->pointer++;
+                }
+
+                public function rewind() {
+                    $this->pointer = 0;
+                }
+
+                public function valid() {
+                    // count() indicates how many items are in the list
+                    return $this->pointer < count($this->items);
+                }
+            }
+
+            // A function that uses iterables
+                function printIterable(iterable $myIterable) {
+                foreach($myIterable as $item) {
+                    echo $item;
+                }
+            }
+
+            // Use the iterator as an iterable
+            $iterator = new MyIterator(["a", "b", "c"]);
+            printIterable($iterator);
+        ?>
+
     </body>
 </html>
