691cbf9815621ad6d62aaf0d64ac341b202d0d55
Philip <philiposun@gmail.com>
2023-06-06 00:55:47 +0100

    php case study

--- PHP OOP/iterables.php
+++ PHP OOP/iterables.php
@@ -10,75 +10,73 @@
     <body>
         <!-- PHP - Using Iterables -->
         <?php
-            // function printItersble(iterable $myIterable) {
-            //     foreach ($myIterable as $item) {
-            //         echo $item;
-            //     }
-            // }
+            function printItersble(iterable $myIterable) {
+                foreach ($myIterable as $item) {
+                    echo $item;
+                }
+            }
 
-            // $arr = ["a", "b", "c"];
-            // printItersble($arr);
-            // echo "<br>";
+            $arr = ["a", "b", "c"];
+            printItersble($arr);
+            echo "<br>";
 
-            // // OR
+            // OR
 
-            // function getIterable():iterable {
-            //     return ["d", "e", "f"];
-            // }
+            function getIterable():iterable {
+                return ["d", "e", "f"];
+            }
 
-            // $myIterable = getIterable();
-            // foreach ($myIterable  as $item) {
-            //     echo $item;
-            // }
+            $myIterable = getIterable();
+            foreach ($myIterable  as $item) {
+                echo $item;
+            }
           
         ?>
 
         <!-- PHP - Creating Iterables -->
         <?php
             // Create an Iterator
-            // class MyIterator implements Iterator {
-            //     private $items = [];
-            //     private $pointer = 0;
+            class MyIterator implements Iterator {
+                private $items = [];
+                private $pointer = 0;
 
-            //     public function __construct($items) {
-            //         $this->items = array_values($items);
-            //     }
+                public function __construct($items) {
+                    $this->items = array_values($items);
+                }
 
-            //     public function current() {
-            //         return $this->items[$this->pointer];
-            //     }
+                public function current() {
+                    return $this->items[$this->pointer];
+                }
 
-            //     public function key() {
-            //         return $this->pointer;
-            //     }
+                public function key() {
+                    return $this->pointer;
+                }
 
-            //     public function next() {
-            //         $this->pointer++;
-            //     }
+                public function next() {
+                    $this->pointer++;
+                }
 
-            //     public function rewind() {
-            //         $this->pointer = 0;
-            //     }
+                public function rewind() {
+                    $this->pointer = 0;
+                }
 
-            //     public function valid() {
-            //         return isset($this->items[$this->pointer]);
-            //     }
-            // }
+                public function valid() {
+                    return isset($this->items[$this->pointer]);
+                }
+            }
 
-            // function printIterable(iterable $myIterable) {
-            //     foreach ($myIterable as $item) {
-            //         echo $item;
-            //     }
-            // }
+            function printIterable(iterable $myIterable) {
+                foreach ($myIterable as $item) {
+                    echo $item;
+                }
+            }
 
-            // $iterator = new MyIterator(["a", "b", "c"]);
-            // $iterator->rewind();
-            // printIterable($iterator);
+            $iterator = new MyIterator(["a", "b", "c"]);
+            $iterator->rewind();
+            printIterable($iterator);
 
         ?>
 
-        <?php
-            echo phpinfo();
-        ?>
+
     </body>
 </html>
